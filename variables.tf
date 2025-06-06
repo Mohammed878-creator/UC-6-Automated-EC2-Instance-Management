afef8a6206cbf95942b7256329e243f93c3535f5
Mohammed878-creator <mdshaik878@gmail.com>
2025-06-06 15:28:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "instance_id" {
 
 variable "start_schedule" {
    type = string
-   default = "cron(15 05 ? * MON-FRI *)"
+   default = "cron(3 10 ? * MON-FRI *)"
 }
 
 variable "stop_schedule" {
   type = string
-  default = "cron(10 05 ? * MON-FRI *)"
+  default = "cron(0 10 ? * MON-FRI *)"
 }
 
 variable "name_prefix" {
